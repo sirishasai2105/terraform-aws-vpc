745d276d128779c196b0ec303ec082dd19b18496
siri123 <saisirishayarra692@gmail.com>
2024-10-14 11:28:32 +0530

    files

--- data.tf
+++ data.tf
@@ -4,3 +4,11 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default" {
     default = true
 }
+data "aws_route_table" "main" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
+
